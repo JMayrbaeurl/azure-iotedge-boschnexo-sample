7e3aa25ae368cc8ed6db60fe15ad2d18d1e202cf
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2019-04-15 13:44:23 +0200

    Added swagger support to edge module

--- nexoEdgeSolution/modules/nexoPublisher/src/main/java/com/microsoft/samples/nexo/edgemodule/App.java
+++ nexoEdgeSolution/modules/nexoPublisher/src/main/java/com/microsoft/samples/nexo/edgemodule/App.java
@@ -10,13 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @RestController
 public class App implements CommandLineRunner {
 
@@ -59,5 +67,14 @@ public class App implements CommandLineRunner {
         } else {
             logger.debug("Nothing to publish. Message is empty");
         }
+    }
+
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.microsoft.samples.nexo.edgemodule"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
     }
 }
