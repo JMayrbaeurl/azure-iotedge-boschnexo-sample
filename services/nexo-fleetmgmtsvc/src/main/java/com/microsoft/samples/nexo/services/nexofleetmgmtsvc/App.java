6e317141f94cdafc65d3f14d6491e1c0441865ff
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2019-10-21 09:13:39 +0200

    Added swagger support with ui

--- services/nexo-fleetmgmtsvc/src/main/java/com/microsoft/samples/nexo/services/nexofleetmgmtsvc/App.java
+++ services/nexo-fleetmgmtsvc/src/main/java/com/microsoft/samples/nexo/services/nexofleetmgmtsvc/App.java
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 @RestController
 public class App {
@@ -95,4 +102,13 @@ public class App {
 
 		return result;
 	}
+
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.microsoft.samples.nexo.services.nexofleetmgmtsvc"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 }
