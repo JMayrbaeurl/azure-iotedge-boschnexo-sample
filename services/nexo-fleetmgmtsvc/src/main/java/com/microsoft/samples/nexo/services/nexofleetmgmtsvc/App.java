ecf1d6ce8746e17ec4b3a914a5b255858197d913
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2019-11-14 11:53:24 +0100

    Added more swagger documentation

--- services/nexo-fleetmgmtsvc/src/main/java/com/microsoft/samples/nexo/services/nexofleetmgmtsvc/App.java
+++ services/nexo-fleetmgmtsvc/src/main/java/com/microsoft/samples/nexo/services/nexofleetmgmtsvc/App.java
@@ -24,12 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.Example;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Api(value = "Bosch Rexroth Nexo Fleet management service", description = "Sample implementation of Nexo fleet management operations")
 @EnableSwagger2
 @SpringBootApplication
 @RestController
@@ -56,15 +65,18 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
+	@ApiOperation(value = "Send Bosch Rexroth Open protocol command to nexo device", response = String.class, consumes = "text/plain")
 	@RequestMapping(value = "/devices/{id}/rop", method = RequestMethod.POST, consumes = { "text/plain" })
 	@ResponseBody
-	public String callNexo(@PathVariable String id, @RequestBody String pBody) {
+	public String callNexo(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id, 
+		@ApiParam(value="Bosch Rexroth Open protocol command", required = true, example="00200001001000000000 ") @RequestBody String ropcommand) {
 
-		logger.info("Raw message call to device '" + id + "' and message '" + pBody + "'");
+		logger.info("Raw message call to device '" + id + "' and message '" + ropcommand + "'");
 
-		return this.serviceClient.callNexo(id, pBody);
+		return this.serviceClient.callNexo(id, ropcommand);
 	}
 
+	@ApiOperation(value="Returns all registered Nexo devices (classified with tags)", response = String.class)
 	@RequestMapping(value = "/devices", method = RequestMethod.GET)
 	@ResponseBody
 	public String getNexoDevices() {
@@ -75,17 +87,19 @@ public class App {
 	}
 
 
+	@ApiOperation(value = "Returns the lock state of the specified nexo device", response = String.class)
 	@RequestMapping(value = "/devices/{id}/lockstate", method = RequestMethod.GET)
 	@ResponseBody
-	public String getNexoDeviceLockState(@PathVariable String id) {
+	public String getNexoDeviceLockState(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id) {
 
 		logger.info("Get Nexo device '" + id + "' lock state");
 
 		return this.serviceClient.getLockStateOfNexo(id);
 	}
 
+	@ApiOperation(value = "Locks the specified nexo device")
 	@RequestMapping(value = "/devices/{id}/lock", method = RequestMethod.POST)
-	public void lockDevice(@PathVariable String id) {
+	public void lockDevice(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id) {
 
 		logger.info("Locking Nexo device '" + id + "'");
 
@@ -93,8 +107,9 @@ public class App {
 			logger.error("Could not lock Nexo device '" + id + "'");
 	}
 
+	@ApiOperation(value = "Unlocks the specified nexo device")
 	@RequestMapping(value = "/devices/{id}/unlock", method = RequestMethod.POST)
-	public void unlockDevice(@PathVariable String id) {
+	public void unlockDevice(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id) {
 
 		logger.info("Unlocking Nexo device '" + id + "'");
 
@@ -102,25 +117,29 @@ public class App {
 			logger.error("Could not unlock Nexo device '" + id + "'");
 	}
 
+	@ApiOperation(value = "Returns the last tightening results of the specified nexo device")
 	@RequestMapping(value = "/devices/{id}/latest", method = RequestMethod.GET)
-	public String getLastestTighteningProcessInfo(@PathVariable String id) {
+	public String getLastestTighteningProcessInfo(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id) {
 
 		logger.info("Get latest tightening process info file for '" + id);
 
 		return this.serviceClient.readLatestTighteningProcessInfo(id);
 	}
 
+	@ApiOperation(value = "Returns the list of program numbers of the nexo device")
 	@RequestMapping(value = "/devices/{id}/programs", method = RequestMethod.GET)
 	@ResponseBody
-	public String getPrograms(@PathVariable String id) {
+	public String getPrograms(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id) {
 
 		logger.info("Get programs for '" + id);
 
 		return this.serviceClient.readPrograms(id);
 	}
 
+	@ApiOperation(value = "Selects the specified program number on the nexo device")
 	@RequestMapping(value = "/devices/{id}/programs/{prognum:[\\d]+}", method = RequestMethod.POST)
-	public void selectProgram(@PathVariable String id, @PathVariable int prognum) {
+	public void selectProgram(@ApiParam(value="IoT Hub device id of nexo device", required = true) @PathVariable String id, 
+		@ApiParam(value="Program number", required = true) @PathVariable int prognum) {
 
 		logger.info("Select program number '" + prognum + "' on device '" + id + "'");
 
@@ -145,8 +164,19 @@ public class App {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.microsoft.samples.nexo.services.nexofleetmgmtsvc"))
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.any()).build().apiInfo(apiEndPointsInfo());
 	}
+
+	private ApiInfo apiEndPointsInfo() {
+
+        return new ApiInfoBuilder().title("Bosch Rexroth Nexo Fleet management service API")
+            .description("Sample implementation of the Nexo Fleet management REST API")
+            .contact(new Contact("Jürgen Mayrbäurl", "www.microsoft.com", "jurgenma@microsoft.com"))
+            .license("MIT")
+            .licenseUrl("https://github.com/JMayrbaeurl/azure-iotedge-boschnexo-sample/blob/master/LICENSE")
+            .version("1.0.0")
+            .build();
+    }
 
 	@Bean
 	public WebSecurityConfigurerAdapter webSecurityConfigurer() {
